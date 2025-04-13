26b338ecff8c38b29e1d8ee71f8982d3496c3fb3
Artsandy Abing <artsandyabing@gmail.com>
2025-04-13 21:15:04 +0800

    updated

--- chat_support_server/config/database.js
+++ chat_support_server/config/database.js
@@ -1,10 +1,6 @@
 require("dotenv").config(); // Load dotenv
-
-import { Sequelize } from "sequelize";
-import config from "./config";
-
-// Define the type for the dialect
-type ValidDialect = "mysql" | "postgres" | "sqlite" | "mssql";
+const { Sequelize } = require("sequelize");
+const config = require("./config");
 
 const environment = process.env.NODE_ENV;
 const DB_DIALECT = config[environment].dialect;
@@ -13,17 +9,16 @@ const DB_USER = config[environment].username;
 const DB_PASS = config[environment].password;
 const DB_NAME = config[environment].database;
 
-// Initialize your Sequelize instance
 const connection = new Sequelize(
   DB_NAME,
   DB_USER,
   DB_PASS,
   {
-    dialect: `${DB_DIALECT}` as ValidDialect,
+    dialect: DB_DIALECT,
     host: DB_HOST,
     timezone: "+08:00",
     logging: false,
   }
 );
 
-export default connection;
+module.exports = connection;
