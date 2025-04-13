2808b2f84faaa268cd198a558ea75385f8cadb9e
Artsandy Abing <artsandyabing@gmail.com>
2025-04-13 22:22:34 +0800

    updated

--- chat_support_server/server.ts
+++ chat_support_server/server.ts
@@ -3,16 +3,14 @@ import { LOCAL } from "./constants";
 
 const start = async () => {
   const environment = process.env.NODE_ENV || "local";
-  console.log("Environment:", environment);
+  console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 
   try {
-    // await database.sync();
-    // await stasherAPI.initializeStasher(fastify);
     if (environment !== LOCAL) {
       fastify.listen({ port: process.env.PORT }, "0.0.0.0", (err: any) => {
         if (err) {
           console.error("Error starting server:", err);
-          process.exit(1); // Exit the process if there's an error starting the server
+          process.exit(1);
         }
 
         console.log(`Server is running on port ${process.env.PORT}`);
