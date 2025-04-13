50a7a4c40737df887e71f2e23c51d2c5eced2c8f
Artsandy Abing <artsandyabing@gmail.com>
2025-04-13 21:27:56 +0800

    updated

--- chat_support_server/config/database.js
+++ chat_support_server/config/database.js
@@ -8,6 +8,8 @@ const DB_USER = process.env.DB_LOCAL_USER;
 const DB_PASS = process.env.DB_LOCAL_PASS;
 const DB_NAME = process.env.DB_LOCAL_NAME;
 
+console.log(DB_NAME, DB_PASS, DB_USER, DB_HOST, DB_DIALECT)
+
 const connection = new Sequelize(
   DB_NAME,
   DB_USER,
